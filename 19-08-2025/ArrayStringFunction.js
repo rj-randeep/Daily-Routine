d6a04152f663f1b7b78d4a9a0e42a4e5235cd22a
Randeep <Randeepjangra687@gmail.com>
2025-08-19 13:41:12 +0530

    Work over the arrays and functions

---
+++ 19-08-2025/ArrayStringFunction.js
@@ -0,0 +1,69 @@
+// function greet(name){
+//     console.log(`Hi ${name}, welcome to our site`);
+// }
+
+// greet("Randeep") 
+
+// function lastdigit(num1, num2) {
+//     let answer;
+//     let a = lastIndexOf(num1);
+//     let b = lastIndexOf(num2);
+
+//     if (a == b) {
+//         answer = 'true';
+//     }
+//     else {
+//         answer = 'false';
+//     }
+
+//     return answer;
+
+// }
+
+// lastdigit(123,123);
+
+
+// function lastDigit(num1, num2) {
+//     // Get last digit of each number
+//     const last1 = Math.floor(num1/10);
+//     const last2 = Math.floor(num2/10);
+//     // Compare and return result
+//     console.log(last1, last2);
+// }
+
+// lastDigit(123, 12543);
+
+
+//............Arrays...................
+
+
+const arr = [1,2,3,4,5,6,7,8,9,10];
+
+const array = new Array(1,2,3,4,5)
+
+// console.log(array.indexOf(3))
+
+// console.log(array.includes(3))
+
+// console.log(array.slice(1))
+
+// console.log(array.slice(-1))
+
+// array.splice(2,0,12);
+
+// console.log(array)
+
+// let mergedArray = arr.concat(array);
+
+// console.log(mergedArray);
+
+
+/* ******   LOOPS IN ARRAY   ******* */
+
+
+// const names = ["Randeep", "Ravi", 'Ankul', 'Dhiraj']
+
+// for(let i=0; i < names.length; i++){
+//     console.log(`${names[i]} index number is ${i}`)
+// }
+
