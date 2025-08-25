32c7624901fac65f6a94cb20758c0fe2ff371755
Randeep <Randeepjangra687@gmail.com>
2025-08-25 13:04:36 +0530

    Adding the work of 25th August on DOM

--- 24-08-2025/oops.js
+++ 24-08-2025/oops.js
@@ -47,51 +47,121 @@
 
 
 
-class Vehicle {
-    // Constructor to initialize properties
+// class Vehicle {
+//     // Constructor to initialize properties
 
-    #registrationNumber;
+//     #registrationNumber;
 
-    constructor(make, model, year, registrationNumber) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-        this.#registrationNumber = registrationNumber;
+//     constructor(make, model, year, registrationNumber) {
+//         this.make = make;
+//         this.model = model;
+//         this.year = year;
+//         this.#registrationNumber = registrationNumber;
+//     }
+
+//     // Method to display vehicle information
+//     displayInfo() {
+//         console.log(`Vehicle Info: ${this.year} ${this.make} ${this.model}`);
+//     }
+// }
+
+
+// class Car extends Vehicle {
+//     // Constructor to initialize properties
+//     constructor(make, model, year, registrationNumber, doors) {
+//         super(make, model, year, registrationNumber);
+//         this.doors = doors;
+//     }
+
+//     // Method to display car information
+//     displayInfo() {
+//         super.displayInfo();
+//         console.log(`Number of doors: ${this.doors}`);
+//     }
+// }
+
+
+// function Truck(make, model, year, registrationNumber, payloadCapacity) {
+//     Vehicle.call(this, make, model, year, registrationNumber);
+//     this.payloadCapacity = payloadCapacity;
+// }
+
+// // Inherit from Vehicle
+// Truck.prototype = Object.create(Vehicle.prototype);
+// Truck.prototype.constructor = Truck;
+
+// // Method to display truck information
+// Truck.prototype.displayInfo = function() {
+//     Vehicle.prototype.displayInfo.call(this);
+//     console.log(`Payload capacity: ${this.payloadCapacity} tons`);
+// };
+
+
+//Do not alter the starter code
+function main(){
+
+  //Create Character constructor function
+    function Character(health, strength, agility) {
+        this.health = health;
+        this.strength = strength;
+        this.agility = agility;
+  }
+  
+  //Create Warrior ,Mage, Enemy and Archer constructor function inheriting Character
+    function Warrior(weaponType, health, strength, agility) {
+        Character.call(this, health, strength, agility);
+        this.weaponType = weaponType;
     }
+    Warrior.prototype = Object.create(Character.prototype);
+    Warrior.prototype.constructor = Warrior;
 
-    // Method to display vehicle information
-    displayInfo() {
-        console.log(`Vehicle Info: ${this.year} ${this.make} ${this.model}`);
+        
+    function Mage(spellType, health, strength, agility) {
+        Character.call(this, health, strength, agility);
+        this.spellType = spellType;
     }
-}
+    Mage.prototype = Object.create(Character.prototype);
+    Mage.prototype.constructor = Mage;
 
-
-class Car extends Vehicle {
-    // Constructor to initialize properties
-    constructor(make, model, year, registrationNumber, doors) {
-        super(make, model, year, registrationNumber);
-        this.doors = doors;
+    function Archer(arrowType, health, strength, agility) {
+        Character.call(this, health, strength, agility);
+        this.arrowType = arrowType;
     }
+    Archer.prototype = Object.create(Character.prototype);
+    Archer.prototype.constructor = Archer;
 
-    // Method to display car information
-    displayInfo() {
-        super.displayInfo();
-        console.log(`Number of doors: ${this.doors}`);
+
+    function Hero(specialAbility, health, strength, agility, weaponType, spellType) {
+        Character.call(this, health, strength, agility);
+        this.weaponType = weaponType;
+        this.spellType = spellType;
+        this.specialAbility = specialAbility;
     }
-}
+    Hero.prototype = Object.create(Character.prototype);
+    Hero.prototype.constructor = Hero;
+    
+    function Enemy(enemyType, health, strength, agility) {
+        Character.call(this, health, strength, agility)
+        this.enemyType = enemyType;
+    }
+    Enemy.prototype = Object.create(Character.prototype);
+    Enemy.prototype.constructor = Enemy;
 
-
-function Truck(make, model, year, registrationNumber, payloadCapacity) {
-    Vehicle.call(this, make, model, year, registrationNumber);
-    this.payloadCapacity = payloadCapacity;
-}
-
-// Inherit from Vehicle
-Truck.prototype = Object.create(Vehicle.prototype);
-Truck.prototype.constructor = Truck;
-
-// Method to display truck information
-Truck.prototype.displayInfo = function() {
-    Vehicle.prototype.displayInfo.call(this);
-    console.log(`Payload capacity: ${this.payloadCapacity} tons`);
-};
+   
+  //Create constructor function for Hero inheriting Hero and Mage
+  
+  let warrior1 = new Warrior(100, 50, 30, "sword"); 
+  let mage1 = new Mage(80, 20, 50, "fireball");
+  let archer1 = new Archer(90, 40, 40, "poison");
+  let hero1 = new Hero(120, 60, 40, "axe", "ice", "double damage");
+  let enemy1 = new Enemy(200, 80, 20, "goblin");
+  
+  console.log(warrior1);
+  console.log(mage1);
+  console.log(archer1);
+  console.log(hero1);
+  console.log(enemy1);
+  
+  return {Character,Hero,Warrior,Mage,Archer,Enemy};
+  }
+  main();
